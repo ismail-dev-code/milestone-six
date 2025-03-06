bb123b66371db5fa63ecfb035b8478ac52993a13
ismail-dev-code <ismail.online0385678@gmail.com>
2025-03-06 21:13:46 +0600

    module-32 added

---
+++ data.js
@@ -0,0 +1,12 @@
+// primitive data type *ei data type shudhumatro ekta specific data rakhte pare
+let person = "faisal haque";
+let newPerson = person;
+person = "hero alom";
+console.log(newPerson);
+
+
+// non-primitive data type *ei data type multiple data type rakhte  pare
+let person2 = { name: "faisal" };
+let newPerson2 = person2;
+person2.name = "janina";
+console.log(newPerson2);
