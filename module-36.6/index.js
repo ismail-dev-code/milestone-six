5b4b31a92780828dd14be839dc2e4eb53fd081b4
ismail-dev-code <ismail.online0385678@gmail.com>
2025-03-20 15:56:19 +0600

    try js added

--- module-36.6/index.js
+++ module-36.6/index.js
@@ -1,7 +1,7 @@
-// const sum = 12;
-// const add = "43";
-// const result = sum + parseInt(add);
-// console.log(result);
+const sum = 12;
+const add = "43";
+const result = sum + parseInt(add);
+console.log(result);
 for(let i =0; i<12; i++){
     console.log(i);
 }
---
+++ module-36.6/try.js
@@ -0,0 +1,17 @@
+const name = "faisal";
+
+try {
+    if(name==="faisaal"){
+        console.log("josh hoice");
+    }
+    else{
+        throw "amar laekha error"
+    }
+//   console.log("bangladehs jpicjar");
+//   console.log(num);
+//   console.log("hobe re eta ");
+} catch (error) {
+  console.error(error);
+} finally {
+  console.log("eta shobshomoy colbe");
+}
